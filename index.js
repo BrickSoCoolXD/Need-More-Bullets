734a08962b3d88c0799b4828b06d5efb987c1292
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2023-08-31 22:52:01 +0700

    fix now can run server but still doesn't show css & js within builit

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const mongoose = require('mongoose')
-const expressSession = require('express-session')
-const flash = require('connect-flash')
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
 
-//Connection Database (MongoDB)
+// Connection to Database (MongoDB)
 mongoose.connect('mongodb+srv://admin:admin@seez.0g8dxw2.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-})
-
-global.loggedIn = null
-
-// Controllers
-const indexController = require('./controllers/indexController')
-const loginController = require('./controllers/loginController')
-const registerController = require('./controllers/registerController')
-const storeUserController = require('./controllers/storeUserController')
-const loginUserController = require('./controllers/loginUserController')
-const logoutController = require('./controllers/logoutController')
-const homeController = require('./controllers/homeController')
+    useUnifiedTopology: true // Add this option to handle deprecation warning
+});
 
 // Middleware
-const redirectIfAuth = require('./middleware/redirectIfAuth')
-const authMiddleware = require('./middleware/authMiddleware')
-
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(flash())
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(flash());
 app.use(expressSession({
-    secret:'secret',
-}))
+    secret: 'secret'
+}));
 
+// Set global variable for authenticated user
 app.use("*", (req, res, next) => {
-    loggedIn = req.session.userId
-    next()
-})
-app.set('view engine', 'ejs')
+    loggedIn = req.session.userId;
+    next();
+});
 
+// Set the view engine
+app.set('view engine', 'ejs');
+
+// Controllers
+const indexController = require('./controllers/indexController');
+const loginController = require('./controllers/loginController');
+const registerController = require('./controllers/registerController');
+const storeUserController = require('./controllers/storeUserController');
+const loginUserController = require('./controllers/loginUserController');
+const logoutController = require('./controllers/logoutController');
+const homeController = require('./controllers/homeController');
+
+// Middleware
+const redirectIfAuth = require('./middleware/redirectIfAuth');
+const authMiddleware = require('./middleware/authMiddleware');
+
+// Routes
 app.get('/', indexController);
 app.get('/home', authMiddleware, homeController);
 app.get('/login', redirectIfAuth, loginController);
